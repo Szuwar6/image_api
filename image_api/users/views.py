189a48f8e302647f750f582c41abb824cfb226f7
Marek <marekszubartowicz@gmail.com>
2023-02-13 21:08:56 +0100

    first

---
+++ image_api/users/views.py
@@ -0,0 +1,20 @@
+from rest_framework import views
+
+class LoginView(views.APIView):
+    def post(self, request, format=None):
+        data = request.data
+
+        username = data.get('username', None)
+        password = data.get('password', None)
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+
+                return Response(status=status.HTTP_200_OK)
+            else:
+                return Response(status=status.HTTP_404_NOT_FOUND)
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
