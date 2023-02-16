3a43d53d58d8787a726d8d097fedf5d36d31209d
Marek <marekszubartowicz@gmail.com>
2023-02-16 22:46:08 +0100

    testing

--- image_api/images/models.py
+++ image_api/images/models.py
@@ -3,38 +3,38 @@ from users.models import UserProfile
 from PIL import Image as PILImage
 
 
-class Image(models.Model):
-    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='images/')
-    uploaded_at = models.DateTimeField(auto_now_add=True)
-    image_basic = models.ImageField(upload_to='images/')
-    image_premium = models.ImageField(upload_to='images/')
-
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-
-        img = PILImage.open(self.image.path)
-        output_size_basic = (200, 200)
-        img.thumbnail(output_size_basic)
-        img.save(self.image_basic.path)
-
-
-        output_size_premium = (400, 400)
-        img = PILImage.open(self.image.path)
-        img.thumbnail(output_size_premium)
-        img.save(self.image_premium.path)
-
-    def __str__(self):
-        return f"{self.image}"
-
 # class Image(models.Model):
 #     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
 #     image = models.ImageField(upload_to='images/')
 #     uploaded_at = models.DateTimeField(auto_now_add=True)
+#     image_basic = models.ImageField(upload_to='images/')
+#     image_premium = models.ImageField(upload_to='images/')
 #
-#     def __str__(self):
-#         return f"{self.image}"
+#     def save(self, *args, **kwargs):
+#         super().save(*args, **kwargs)
 #
+#         img = PILImage.open(self.image.path)
+#         output_size_basic = (200, 200)
+#         img.thumbnail(output_size_basic)
+#         img.save(self.image_basic.path)
+#
+#
+#         output_size_premium = (400, 400)
+#         img = PILImage.open(self.image.path)
+#         img.thumbnail(output_size_premium)
+#         img.save(self.image_premium.path)
+
+    # def __str__(self):
+    #     return f"{self.image}"
+
+class Image(models.Model):
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.image}"
+
 #
 # class ImageBasic(models.Model):
 #     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
@@ -67,3 +67,4 @@ class Image(models.Model):
 #
 #     def __str__(self):
 #         return f"{self.image}"
+
--- image_api/images/views.py
+++ image_api/images/views.py
@@ -1,10 +1,14 @@
+import os
 
 from rest_framework import generics
 from images.models import Image
 from images.serializers import ImageSerializer
 from rest_framework.permissions import IsAuthenticated
+from PIL import Image as PILImage
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.response import Response
 
-
+from users.models import Plan
 
 
 class ImageList(generics.ListAPIView):
@@ -16,6 +20,92 @@ class ImageList(generics.ListAPIView):
         user_profile = user.userprofile
         return Image.objects.filter(user=user_profile)
 
+# class ImageCreate(generics.CreateAPIView):
+#     serializer_class = ImageSerializer
+#     permission_classes = [IsAuthenticated]
+#
+#
+#
+#     def image_create(self, serializer):
+#
+#         user = self.request.user
+#         user_profile = user.userprofile
+#         user_plan = user_profile.plan
+#         if user_plan == "Enterprise":
+#
+#
+#             # user = self.request.user
+#             # user_profile = user.userprofile
+#             # serializer.save(user=user_profile)
+#
+#             image_file = PILImage.open(serializer.instance.image.path)
+#
+#             output_size = (200, 200)
+#             image_200 = image_file.resize(output_size)
+#             image_200_path = serializer.instance.image.path.replace('.', '_200.')
+#             image_200.save(image_200_path)
+#
+#
+#             output_size = (400, 400)
+#             image_400 = image_file.resize(output_size)
+#             image_400_path = serializer.instance.image.path.replace('.', '_400.')
+#             image_400.save(image_400_path)
+#
+#         else:
+#             raise PermissionDenied(detail="Wrong plan")
+
+
 class ImageCreate(generics.CreateAPIView):
     serializer_class = ImageSerializer
     permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        user = self.request.user
+        user_profile = user.userprofile
+        user_plan = user_profile.plan
+
+        if user_plan == Plan.ENTERPRISE:
+            serializer.save(user=user_profile)
+
+            image_file = PILImage.open(serializer.instance.image.path)
+
+            output_size = (200, 200)
+            image_200 = image_file.resize(output_size)
+            image_200_path = serializer.instance.image.path.replace('.', '_200.')
+            image_200.save(image_200_path)
+
+            output_size = (400, 400)
+            image_400 = image_file.resize(output_size)
+            image_400_path = serializer.instance.image.path.replace('.', '_400.')
+            image_400.save(image_400_path)
+
+        elif user_plan == Plan.PREMIUM:
+            serializer.save(user=user_profile)
+
+            image_file = PILImage.open(serializer.instance.image.path)
+
+            output_size = (200, 200)
+            image_200 = image_file.resize(output_size)
+            image_200_path = serializer.instance.image.path.replace('.', '_200.')
+            image_200.save(image_200_path)
+
+            output_size = (400, 400)
+            image_400 = image_file.resize(output_size)
+            image_400_path = serializer.instance.image.path.replace('.', '_400.')
+            image_400.save(image_400_path)
+
+        elif user_plan == Plan.BASIC:
+            serializer.save(user=user_profile)
+
+            image_file = PILImage.open(serializer.instance.image.path)
+
+            output_size = (200, 200)
+            image_200 = image_file.resize(output_size)
+
+            image_200_path = serializer.instance.image.path.replace('.', '_200.')
+            image_200.save(image_200_path)
+            os.remove(serializer.instance.image.path)
+
+
+        else:
+            return Response({"error": "Nieprawidłowy plan"}, status=400)
